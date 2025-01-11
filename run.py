fe5fc9e6700a5aef8fa88819857a23b5dbc4b2c5
녁랴 <62201556+nyeoglya@users.noreply.github.com>
2025-01-12 04:29:23 +0900

    fix: change app to run on 0.0.0.0

--- run.py
+++ run.py
@@ -10,4 +10,4 @@ sys.path.append("./lane_detection/")
 app = create_app()
 app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
