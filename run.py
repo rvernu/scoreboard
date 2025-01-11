f457623d7d997b64853bf1bfe61c3f54340d083d
hegelty <skxodid0305@gmail.com>
2025-01-12 00:47:11 +0900

    Implement API endpoints

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import os
+
 from app import create_app
 import sys
 
@@ -11,6 +13,6 @@ sys.path.append("./lane_detection/")
 sys.path.append("./lane_detection/ultrafastLaneDetector")
 
 app = create_app()
-
+app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
 if __name__ == '__main__':
     app.run(debug=True)
