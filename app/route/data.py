65a4a54daaf91410282640d38bbc3d6b5b1fd3e2
hegelty <skxodid0305@gmail.com>
2025-01-11 19:50:36 +0900

    Change image api to data api

--- app/route/data.py
+++ app/route/data.py
@@ -4,12 +4,13 @@ import time
 from flask import Blueprint, render_template, request
 from werkzeug.utils import secure_filename
 
-image_route = Blueprint('image', __name__, url_prefix='/image')
+data_route = Blueprint('data', __name__, url_prefix='/data')
 
-@image_route.route('/', methods=['POST'])
+@data_route.route('/', methods=['POST'])
 def upload_image():
     try:
-        # Get the image from the request
+        longitude = request.form['longitude']
+        latitude = request.form['latitude']
         f = request.files['image']
         file_hash = str(hash(f))[:3] + str(int(time.time())) + str(random.randint(0, 10000))
         f.save("./tmp/" + secure_filename(file_hash))
