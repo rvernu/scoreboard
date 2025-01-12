64d1ffc198f7022a604d69acd061af758fdd8baa
hegelty <skxodid0305@gmail.com>
2025-01-12 13:04:04 +0900

    Change return encoding

--- run.py
+++ run.py
@@ -10,5 +10,6 @@ app = create_app()
 app.debug = True
 
 app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
+app.config['JSON_AS_ASCII'] = False
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
